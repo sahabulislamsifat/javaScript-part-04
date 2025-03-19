d53db20a2e2909ffb63f6fd9b792d3c3adbee097
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-20 02:53:55 +0600

    explore conditional function

---
+++ condition.js
@@ -0,0 +1,74 @@
+// let age = 18;
+// let age = 19;
+// let age = 31;
+
+// if (age < 18) {
+//   console.log("Your are young!");
+// } else if (age == 18) {
+//   console.log("Now you are adult!");
+// } else if (age > 18 && age < 30) {
+//   console.log("Your are mature!");
+// } else {
+//   console.log("Your are old..");
+// }
+
+// let age = 31;
+// let age = 18;
+// let age = 17;
+// let age = 22
+
+// switch (true) {
+//   case age < 18:
+//     console.log("To young!");
+//     break;
+//   case age === 18:
+//     console.log("You are an adult!");
+//     break;
+//   case age > 18 && age < 30:
+//     console.log("you are mature!");
+//     break;
+//   default:
+//     console.log("old enough");
+// }
+
+// let grade;
+
+// if (score <= 5) {
+//   grade = "E";
+// } else if (score <= 10) {
+//   grade = "F";
+// } else if (score <= 15) {
+//   grade = "G";
+// } else if (score <= 20) {
+//   grade = "H";
+// } else if (score <= 25) {
+//   grade = "I";
+// } else {
+//   grade = "J";
+// }
+
+// console.log(grade);
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Deo",
+//   id: 11542,
+//   fullName: function () {
+//     return this.firstName + "" + this.lastName;
+//   },
+// };
+
+// console.log(person.fullName);
+
+// if (15 > 15) {
+//   console.log("Yes, It is");
+// } else {
+//   console.log("No it is not");
+// }
+
+let year = 2020;
+
+if (true) {
+  console.log(year);
+  let year = 2021;
+}
