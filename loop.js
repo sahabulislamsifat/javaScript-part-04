5b55dc986f406efdb462bc45d071e7d4e49ec9f2
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-20 04:19:36 +0600

    temporary closed

---
+++ loop.js
@@ -0,0 +1,45 @@
+// For Loop
+
+// const letters = ["A", "B", "C", "E", "F", "G", "H"];
+
+// for (let i = 0; i < letters.length; i++) {
+//   console.log(i);
+// }
+
+// console.log("Loop Done..");
+
+// var i = 5;
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// let i = 5;
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// console.log("This is I", i);
+
+// For IN Loop ****
+
+// const person = {
+//   firstName: "Sahabul Islam",
+//   lastName: "Sifat",
+//   age: 22,
+// };
+
+// for (let x in person) {
+//   console.log(x);
+//   console.log(person[x]);
+// }
+
+const numbers = [11, 45, 64, 87, 36, 45];
+
+for (let number in numbers) {
+  console.log(number);
+  //   console.log(numbers[number]);
+}
