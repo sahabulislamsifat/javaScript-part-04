91290a4881560adba5a1f25e443c2a867fecd046
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-20 01:27:54 +0600

    ternary operator

---
+++ comparisons.js
@@ -0,0 +1,51 @@
+// const x = 5;
+// const y = "5";
+
+// console.log(x == y);
+// console.log(x === y);
+// console.log(x !== y);
+// console.log(x !== 8);
+// console.log(x !== "5");
+
+// console.log(x < 8);
+// console.log(x >= 8);
+// console.log(x > 8);
+
+// if else
+
+// let age = 18;
+// let age = 17;
+
+// if (age >= 18) {
+//   console.log("Your are adult!");
+// } else {
+//   console.log("Your are not adult!");
+// }
+
+// let age = 18;
+// let age = 17;
+// if (age == 16 || age == 15 || age == 17) {
+//   console.log("Your are not adult!");
+// }
+
+// if (age > 16 && age < 20 && age > 17) {
+//   console.log("Your are  adult!");
+// }
+
+// Ternary Operator
+
+// let result = age >= 18 ? "Your are young" : "Your are child.";
+
+// console.log(result);
+
+// let age = 17;
+// let age = 20;
+
+// const result =
+//   age >= 18
+//     ? age < 30
+//       ? "you are a young"
+//       : "you are a old"
+//     : "you are a child";
+
+// console.log(result);
